3c811c3576b80fe510c7d0fab876f6f864b556bf
marcocalligaris <marcocally@gmail.com>
2022-09-15 16:49:04 +0200

    Added images folder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,23 @@
     <title>DC Comics | {{ $title }}</title>
 </head>
 <body>
-
+    <header>
+        <div class="container">
+            <figure>
+                <a href="#"><img src="../assets/img/dc-logo.png" alt="DC Comics logo"></a>
+            </figure>
+            <nav>
+                <ul>
+                    <li v-for="link in links" :key="link.id">
+                        <a
+                            :href="link.url"
+                            :class="{'active': link.active }"
+                            >
+                            {{ link.text }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </body>
 </html>
