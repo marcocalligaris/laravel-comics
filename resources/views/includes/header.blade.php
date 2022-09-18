a705b305e85c5ba718b432ed03b8603539d299d8
marcocalligaris <marcocally@gmail.com>
2022-09-18 10:10:04 +0200

    Commit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,22 +1,22 @@
 <header>
-        <div class="container">
-            <figure>
-                <a href="#"><img src="{{ asset('images/dc-logo.png') }}" alt="DC Comics logo"></a>
-            </figure>
-            <nav>
-                <ul>
-                    <li><a href="{{ route('home') }}">HOME</a></li>
-                    <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
-                    <li><a href="{{ route('comics') }}">COMICS</a></li>
-                    <li><a href="{{ route('movies') }}">MOVIES</a></li>
-                    <li><a href="{{ route('tv') }}">TV</a></li>
-                    <li><a href="{{ route('games') }}">GAMES</a></li>
-                    <li><a href="{{ route('collectibles') }}">COLLECTIBLES</a></li>
-                    <li><a href="{{ route('videos') }}">VIDEOS</a></li>
-                    <li><a href="{{ route('fans') }}">FANS</a></li>
-                    <li><a href="{{ route('news') }}">NEWS</a></li>
-                    <li><a href="{{ route('shop') }}">SHOP</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <div class="container">
+        <figure>
+            <a href="#"><img src="{{ asset('images/dc-logo.png') }}" alt="DC Comics logo"></a>
+        </figure>
+        <nav>
+            <ul>
+                <li><a href="{{ route('home') }}">HOME</a></li>
+                <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
+                <li><a href="{{ route('comics') }}">COMICS</a></li>
+                <li><a href="{{ route('movies') }}">MOVIES</a></li>
+                <li><a href="{{ route('tv') }}">TV</a></li>
+                <li><a href="{{ route('games') }}">GAMES</a></li>
+                <li><a href="{{ route('collectibles') }}">COLLECTIBLES</a></li>
+                <li><a href="{{ route('videos') }}">VIDEOS</a></li>
+                <li><a href="{{ route('fans') }}">FANS</a></li>
+                <li><a href="{{ route('news') }}">NEWS</a></li>
+                <li><a href="{{ route('shop') }}">SHOP</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
